a1768d4bb313b72643f9364be6dfb5023f84b024
Krishna <58246186+KrishnaKafley21@users.noreply.github.com>
2022-09-30 07:28:26 +1000

    Update Youtube Downloader.sh

--- Youtube Downloader.sh
+++ Youtube Downloader.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-youtube-dl -o "/home/pi/Music/%(title)s.%(ext)s" -r 5000M -x --audio-format mp3 -------https://youtube.com/
+DEST_FOLDER = "/home/pi/Music/%(title)s.%(ext)s"
+URL = https://youtube.com/playlist?list=PLPStEyPAxfzN3r86XUvwhpyEMAAkqlggB
+
+youtube-dl -o $DEST_FOLDER -r 5000M -x --audio-format mp3 $URL
